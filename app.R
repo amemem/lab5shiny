8855b40f95fb11ac7a6b2dcaf6c6edcb72f9c917
amemem <ameme698@student.liu.se>
2021-09-28 19:21:13 +0200

    Implement Shiny app

--- app.R
+++ app.R
@@ -1,49 +1,63 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(lab5package)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+val = val2014()
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+ui = fluidPage(
+    titlePanel("2014 Swedish election: county results"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput(
+                "county",
+                "County:",
+                choices = c(
+                    "Blekinge",
+                    "Dalarna",
+                    "Gotland",
+                    "Gavleborg",
+                    "Halland",
+                    "Jamtland",
+                    "Jonkoping",
+                    "Kalmar",
+                    "Kronoberg",
+                    "Norrbotten",
+                    "Skane",
+                    "Stockholm",
+                    "Sodermanland",
+                    "Uppsala",
+                    "Varmland",
+                    "Vasterbotten",
+                    "Vasternorrland",
+                    "Vastmanland",
+                    "vastergotland",
+                    "Orebro",
+                    "Ostergotland"
+                )
+            ),
+            hr(),
+            helpText("Source: Valmyndigheten (2014)")
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("countyPlot")
         )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+server = function(input, output) {
+    output$countyPlot = renderPlot({
+        selectCounty = val$county(input$county)
+        bp = barplot(
+            selectCounty,
+            main = input$county,
+            xlab = "Political party",
+            ylab = "Percent",
+            ylim = c(0, 50),
+            col = c("#66BEE6", "#63A91D", "#3399FF", "#1B5CB1", "#FF0000",
+                    "#C40000", "#008000", "#4E83A3", "#cd0067", "grey")
+        )
+        axis(2, seq(0, 50, 5))
+        text(bp, 0, format(round(selectCounty, 2), nsmall = 2), pos = 3)
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
